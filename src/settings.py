f2b79dd2cdccdc1a054ae771119022a840d19e11
alina <khomkolovaalina@gmail.com>
2024-07-27 11:54:40 +0200

    Исправила ошибку связанную с путями.

--- src/settings.py
+++ src/settings.py
@@ -1,2 +1,4 @@
-DATABASE_INI = '/home/alina/PycharmProjects/course_5/data/database.ini'
-CREATE_TABLES = '/home/alina/PycharmProjects/course_5/src/create_tables.sql'
+import os
+
+DATABASE_INI = os.path.abspath('data/database.ini')
+CREATE_TABLES = os.path.abspath('src/create_tables.sql')
